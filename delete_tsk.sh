bb13134d85ef71c0e6b1a9f6ded075e6ff48eaf1
mandyliu0305 <mandyliu0305@users.noreply.github.com>
2017-10-29 17:32:31 -0400

    Create delete_tsk.sh

---
+++ delete_tsk.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#can setup with crontab on 8am everyday: * 8 * * * delete_tsk/sh 2 >> log
+DIR=/home/mandy/workspace
+OUTPUT=$DIR/scripts/log/tsk.txt
+date >> $OUTPUT
+find $DIR -name *.tsk -print -delete >> $OUTPUT
