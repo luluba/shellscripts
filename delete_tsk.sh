638d19dde07ef8c046df25509cbcd16089fadea2
mandyliu0305 <mandyliu0305@users.noreply.github.com>
2017-10-30 10:36:16 -0400

    Update delete_tsk.sh

--- delete_tsk.sh
+++ delete_tsk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#can setup with crontab on 8am everyday: * 8 * * * delete_tsk/sh 2 >> log
+#can setup with crontab on 8am everyday: 0 8 * * * delete_tsk/sh 2 >> log
 DIR=/home/mandy/workspace
 OUTPUT=$DIR/scripts/log/tsk.txt
 date >> $OUTPUT
